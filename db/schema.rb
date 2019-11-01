ef5819bb2fee618267a90cb85c805d3186efce86
Kartik Raina <raina.kartik15@gmail.com>
2019-11-02 00:17:21 +0530

    add user password facility to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_192631) do
+ActiveRecord::Schema.define(version: 2019_11_01_183933) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_192631) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
